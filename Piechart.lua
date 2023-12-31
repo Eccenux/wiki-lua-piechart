ef1662eeff758192afdfc6c6361ed9c6207ad571
Nux <Nux@fake.wikipedia.org>
2023-12-31 17:54:53 +0000

    value fix

--- Piechart.lua
+++ Piechart.lua
@@ -1,4 +1,11 @@
 local p = {}
+--[[
+	Debug:
+	
+	local json_data = '[{"label": "k: $v", "value": 33.1}, {"label": "m: $v", "value": -1}]'
+	local html = p.renderPie(json_data)
+	mw.logObject(html)
+]]
 
 --[[
 	Piechart.
@@ -8,9 +15,8 @@ local p = {}
         { "label": "k: $v", "value": 33.1  },
         { "label": "m: $v", "value": -1  },
     ]
-    
     where $v is a formatted label
-    
+
     TODO:
     - 2-element pie chart
         - read json
@@ -32,7 +38,7 @@ local p = {}
 ]] 
 function p.pie(frame)
 	local json_data = trim(frame.args[1])
-	local html = renderPie(json_data)
+	local html = p.renderPie(json_data)
 	return html
 end
 
@@ -41,13 +47,13 @@ end
 	
 	@param json_data JSON string with pie data.
 ]]
-function renderPie(json_data)
+function p.renderPie(json_data)
 	local html = ""
 	local sum = 0;
 	local data = mw.text.jsonDecode( json_data )
 	for _, entry in ipairs(data) do
 	    html = html .. renderSlice(entry, sum)
-    	sum = sum + value
+    	sum = sum + entry.value
 	end
 	return html
 end
@@ -59,14 +65,14 @@ end
 	@param entry Sum up-until now (in 2-pie that would be % for first value).
 ]]
 function renderSlice(entry, sum)
-	-- local label = entry.label:gsub("%$v", entry.value)
-	local label = string.format("%.1f", entry.value) .. "%"
-	
 	local value = entry.value
 	if entry.value < 0 then
         value = 100 - sum
 	end
 
+	-- local label = entry.label:gsub("%$v", value)
+	local label = string.format("%.1f", value) .. "%"
+	
 	local html =  "<p>" .. "Label: " .. label  .. "; value: " .. value  .. "</p>"
 	
 	return html
