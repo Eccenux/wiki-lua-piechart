34ca501cd5dc98236ae33b5e90b016547ebf16c5
eccenux <eccenux+git@gmail.com>
2025-08-02 20:22:05 +0200

    basic wikiploy

---
+++ wikiploy-dev.mjs
@@ -0,0 +1,38 @@
+import { Wikiploy, setupSummary } from 'wikiploy';
+
+import * as botpass from './bot.config.mjs';
+const ployBot = new Wikiploy(botpass);
+
+// run asynchronously to be able to wait for results
+(async () => {
+	// custom summary from a prompt
+	await setupSummary(ployBot);
+
+	// push out file(s) to wiki
+	const configs = [];
+	
+	let site = 'en.wikipedia.org';
+	
+	configs.push(new DeployConfig({
+		src: 'tpl.Piechart.en.css',
+		dst: 'Template:Pie chart/sandbox/styles.css',
+		site,
+	}));
+	configs.push(new DeployConfig({
+		src: 'tpl.Piechart.en.mediawiki',
+		dst: 'Template:Pie chart/sandbox',
+		site,
+	}));
+	configs.push(new DeployConfig({
+		src: 'Piechart.lua',
+		dst: 'Module:Piechart/sandbox',
+		site,
+	}));
+
+	// deploy
+	await ployBot.deploy(configs);
+
+})().catch(err => {
+	console.error(err);
+	process.exit(1);
+});
---
+++ wikiploy.mjs
@@ -0,0 +1,24 @@
+import { Wikiploy, setupSummary } from 'wikiploy';
+
+import * as botpass from './bot.config.mjs';
+const ployBot = new Wikiploy(botpass);
+
+// run asynchronously to be able to wait for results
+(async () => {
+	// custom summary from a prompt
+	await setupSummary(ployBot);
+
+	// push out file(s) to wiki
+	// const configs = [];
+	// // dev version
+	// addConfig(configs, 'en.wikipedia.org');
+	// // release versions
+	// addConfigRelease(configs, 'pl.wikipedia.org');
+	// addConfigRelease(configs, 'en.wikipedia.org');
+
+	// await ployBot.deploy(configs);
+
+})().catch(err => {
+	console.error(err);
+	process.exit(1);
+});
